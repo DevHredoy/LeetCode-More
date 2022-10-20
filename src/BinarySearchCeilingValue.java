cba7c549064eb97616fc3070103e345185257dcd
rafiul <hredoy.rafiul@gmail.com>
2022-10-20 07:09:13 +0600

    revised

--- src/BinarySearchCeilingValue.java
+++ src/BinarySearchCeilingValue.java
@@ -1,29 +1,29 @@
 public class BinarySearchCeilingValue {
+    // In this code there will be an array.If the user give one target input in the method "binarySearchTarget(..target..)"
+    //the output shall show the closest equal or greater value in the array.
     //this code is very same like the BinarySearchDemo program except line 10
-    public static  void binarySearchTarget(int[] arr,int target,int lowerBound,int upperBound)
+    public static  boolean binarySearchTarget(int[] arr,int target,int lowerBound,int upperBound)
 
     {
-
-
         if(arr[lowerBound+1]==arr[upperBound])
         {
             System.out.println("the most closest equal or greater value is "+arr[upperBound]);
-            return ;
+            return true;
         }
         int mid=(lowerBound+upperBound)/2;
         if(target==arr[mid])
         {
             System.out.println("the target is in the array");
-            return ;//i want to break the recursion once i find the expected result
+            return true;
         }else if(target==arr[upperBound])
         {
             System.out.println("target is within the array ");
-            return;
+           return true;
         }
         else if (target==arr[lowerBound])
         {
             System.out.println("the target is within the array");
-            return;
+            return true;
         }
         else if(target<arr[mid])
         {
@@ -36,15 +36,14 @@ public class BinarySearchCeilingValue {
         }
 
 
-    }
+  return false;  }
 
 
 
     public static void main(String[] args) {
 
         int[] array=new int[]{2,4,12,16,18,21,23,27,30};
-        int upperbound=array.length-1;
-        binarySearchTarget(array,13,0,upperbound);
+        binarySearchTarget(array,11,0,array.length-1);
 
     }
 }
